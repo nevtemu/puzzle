bc317d64af9145ba9a7520c62a3f053698b3a507
nevtemu <artem.soroka@hotmail.com>
2020-10-09 15:16:40 +0400

    Added timer and turns counter

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 const GRIDS = [[1,1],[1,2],[1,3],[2,3],[3,3],[3,2],[3,1],[2,1]]
-const createBoxes = () => {
-    document.getElementById("winPopUp").classList.toggle("show");
-    document.getElementById('main').innerHTML = "";
-let boxStates = [];
-for (k=0; k<8; k++){
-if (k<2){if (getRandomNumber(1,2)==1){boxStates.push("active")}else{boxStates.push("inactive")}}
-else{
-            if (boxStates[k-2]==boxStates[k-1]){
-                    if(boxStates[k-2]=="active"){boxStates.push("inactive")}
-                    else{boxStates.push("active")}}
-            else{if (getRandomNumber(1,2)==1){boxStates.push("active")}else{boxStates.push("inactive")}}}}
+let turns, time, counter;
 
-for (i=0; i<8; i++){
-document.getElementById('main').innerHTML += `<div name="box" class="${boxStates[i]}" id="box${i}" onclick="userMove(this.id)" style="grid-row:${GRIDS[i][0]}; grid-column: ${GRIDS[i][1]}"></div>`;
-}
-
-}
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min; 
 
 const checkGameStatus = () => {
@@ -25,6 +10,9 @@ boxStates.push(document.getElementById(`box${k}`).getAttribute("class"))
     }
     if (boxStates.includes("inactive")){}
     else{
+        clearInterval (counter);
+        document.getElementById("finalTime").innerHTML = `${convertTime(parseInt(time / 60))}:${convertTime(time % 60)}`
+        document.getElementById("finalTurns").innerHTML = turns;
         document.getElementById("winPopUp").classList.toggle("show");
     }
 }
@@ -40,9 +28,48 @@ const changeBoxStatus = (box) => {
     else {document.getElementById(`box${boxList[j]}`).setAttribute("class", "active")}   
 }}
 
+const countTurns = ()=>{
+    turns++;
+    document.getElementById("turns").innerHTML = turns;
+}
+
 const userMove = (buttonID)=>{
+countTurns();
 changeBoxStatus(buttonID);
 checkGameStatus();
 }
 
+
+
+//+++++++++++++++++++++++
+const convertTime = (value) => { return value > 9 ? value : "0" + value; }
+const countTime = () => {
+  time++;
+  document.getElementById("seconds").innerHTML = convertTime(time % 60);
+  document.getElementById("minutes").innerHTML = convertTime(parseInt(time / 60));
+}
+
+
+
+
+const createBoxes = () => {
+    time = 0;
+    counter = setInterval(countTime, 1000);
+    turns = 0;
+    document.getElementById("winPopUp").classList.toggle("show");
+    document.getElementById('main').innerHTML = "";
+let boxStates = [];
+for (k=0; k<8; k++){
+if (k<2){if (getRandomNumber(1,2)==1){boxStates.push("active")}else{boxStates.push("inactive")}}
+else{
+            if (boxStates[k-2]==boxStates[k-1]){
+                    if(boxStates[k-2]=="active"){boxStates.push("inactive")}
+                    else{boxStates.push("active")}}
+            else{if (getRandomNumber(1,2)==1){boxStates.push("active")}else{boxStates.push("inactive")}}}}
+
+for (i=0; i<8; i++){
+document.getElementById('main').innerHTML += `<div name="box" class="${boxStates[i]}" id="box${i}" onclick="userMove(this.id)" style="grid-row:${GRIDS[i][0]}; grid-column: ${GRIDS[i][1]}"></div>`;
+}
+document.getElementById('main').innerHTML += `<div class="stats" id="stats" style="grid-row:2; grid-column: 2"> <div>Turns: <span id="turns">0</span></div><br><div> Time: <span id="minutes">00</span>:<span id="seconds">00</span> </div></div>`;
+}
 createBoxes();
