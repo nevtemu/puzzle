569a1dbf249d61ffa0ddde9e007fb287168aac48
nevtemu <artem.soroka@hotmail.com>
2020-10-09 10:50:26 +0400

    Added responsive design

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById('main').innerHTML += `<div name="box" class="${boxStates
 }
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min; 
 
-const checkGame = () => {
+const checkGameStatus = () => {
     let boxStates = [];
     for (k=0; k<8; k++){ 
 boxStates.push(document.getElementById(`box${k}`).getAttribute("class"))
@@ -29,20 +29,20 @@ boxStates.push(document.getElementById(`box${k}`).getAttribute("class"))
     }
 }
 
-const changeStatus = (box) => {
+const changeBoxStatus = (box) => {
     let boxList = [];
     let m = parseInt(box.split("").pop());
     if (m==0){boxList=[7,0,1]}
     else if(m==7){boxList=[0,6,7]}
     else{boxList=[m-1,m,m+1]}
     for (j=0; j<=2; j++){
-    if (document.getElementById(`box${boxList[j]}`).getAttribute("class")=="active"){document.getElementById(`box${boxList[j]}`).setAttribute("class", "inactive")}
+    if (document.getElementById(`box${boxList[j]}`).getAttribute("class") =="active"){document.getElementById(`box${boxList[j]}`).setAttribute("class", "inactive")}
     else {document.getElementById(`box${boxList[j]}`).setAttribute("class", "active")}   
 }}
 
 const userMove = (buttonID)=>{
-changeStatus(buttonID);
-checkGame();
+changeBoxStatus(buttonID);
+checkGameStatus();
 }
 
 createBoxes();
